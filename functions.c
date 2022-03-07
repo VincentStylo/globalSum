cee1aa234dc79eeb60d958da2f8212a6ce23bb5f
Vincent Stylo <j.martinez.torres@icloud.com>
2022-03-07 09:32:02 -0500

    Up to Slide 12

--- functions.c
+++ functions.c
@@ -7,8 +7,26 @@
 #include "functions.h"
 
 void global_sum(double *Result, int rank, int size, double my_value)
-{   
-    printf("\n\nINSIDE FUNCTION DOUBLE SUM\n");
-    printf("Welcome, %d: Your value is: %f, your result is: %f \n", rank, my_value, *Result);
+{
     *Result = my_value;
+    double RECIEVER;
+    for (int phase = 0; phase < size-1; phase++)
+    {
+        if (rank == 0 || rank % 2 == 0)
+        {
+            printf("Phase %d P %d recieving from %d, val %.1f \n",phase, rank, rank + 1, my_value);
+            MPI_Recv(&RECIEVER, 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            *Result += RECIEVER;
+            printf("Phase %d P %d sending   to   %d, val %.1f \n",phase, rank, rank + 1, my_value);
+            MPI_Ssend(&my_value, 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD);
+        }
+        else
+        {
+            printf("Phase %d P %d sending   to   %d, val %.1f \n",phase, rank, rank - 1, my_value);
+            MPI_Ssend(&my_value, 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD);
+            printf("Phase %d P %d recieving from %d, val %.1f \n",phase,  rank, rank + 1, my_value);
+            MPI_Recv(&RECIEVER, 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            *Result += RECIEVER;
+        }
+    }
 }
