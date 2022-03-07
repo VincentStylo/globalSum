5b296e659bc78e8d396b8284e1a343ebb526238d
Vincent Stylo <j.martinez.torres@icloud.com>
2022-03-07 13:10:52 -0500

    VERSION BEFORE BINARY MATH

--- functions.c
+++ functions.c
@@ -5,27 +5,35 @@
 #include <mpi.h>
 
 #include "functions.h"
+int phaseSum(int x)
+{
+    return log(x) / log(2);
+}
 
 void global_sum(double *Result, int rank, int size, double my_value)
 {
     *Result = my_value;
     double RECIEVER;
-    for (int phase = 0; phase < size-1; phase++)
+    int STEP;
+    int phaseMax = phaseSum(size);
+    for (int phase = 0; phase < phaseMax; phase++)
     {
+        STEP = (int)pow(2,phase);
         if (rank == 0 || rank % 2 == 0)
         {
-            printf("Phase %d P %d recieving from %d, val %.1f \n",phase, rank, rank + 1, my_value);
-            MPI_Recv(&RECIEVER, 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            printf("Phase %d P %d recieving from %d, val %.1f \n", phase, rank, rank + STEP, my_value);
+            MPI_Recv(&RECIEVER, 1, MPI_DOUBLE, rank + STEP, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             *Result += RECIEVER;
-            printf("Phase %d P %d sending   to   %d, val %.1f \n",phase, rank, rank + 1, my_value);
-            MPI_Ssend(&my_value, 1, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD);
+            printf("Phase %d P %d sending   to   %d, val %.1f \n",phase, rank, rank + STEP, my_value);
+            MPI_Ssend(&my_value, 1, MPI_DOUBLE, rank + STEP, 1, MPI_COMM_WORLD);
         }
         else
         {
-            printf("Phase %d P %d sending   to   %d, val %.1f \n",phase, rank, rank - 1, my_value);
-            MPI_Ssend(&my_value, 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD);
-            printf("Phase %d P %d recieving from %d, val %.1f \n",phase,  rank, rank + 1, my_value);
-            MPI_Recv(&RECIEVER, 1, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("Phase %d P %d sending   to   %d, val %.1f \n",phase ,rank, rank - STEP, my_value);
+            MPI_Ssend(&my_value, 1, MPI_DOUBLE, rank - STEP, 1, MPI_COMM_WORLD);
+            printf("Phase %d P %d recieving from %d, val %.1f \n",phase ,rank,  - STEP, my_value);
+            MPI_Recv(&RECIEVER, 1, MPI_DOUBLE, rank - STEP, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             *Result += RECIEVER;
         }
     }
--- functions.h
+++ functions.h
@@ -1,4 +1,5 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+int phaseSum(int x);
 void global_sum(double* result, int rank, int size, double my_value);
 #endif
